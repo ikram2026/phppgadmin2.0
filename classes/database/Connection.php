2e4050db9bd685e571d999b5efdf154867768657
Charlie Hayes <cosmotic@gmail.com>
2023-09-21 12:00:13 -0500

    Hopefully fixed postgres version check for versions after 14

--- classes/database/Connection.php
+++ classes/database/Connection.php
@@ -76,11 +76,12 @@ class Connection {
 
 		// Detect version and choose appropriate database driver
         switch (substr($version,0,2)) {
-            case '14': return 'Postgres';break;
             case '13': return 'Postgres13';break;
             case '12': return 'Postgres12';break;
             case '11': return 'Postgres11';break;
             case '10': return 'Postgres10';break;
+			// 14 and onward
+			default: return 'Postgres';
         }    
 
 		switch (substr($version,0,3)) {
